67a4c456f07fd570baad228e9fd48c6bcf3c4d15
DemetrioPadre <demetriopadre@gmail.com>
2024-02-07 16:16:27 +0100

    add script / template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- creo uno script dove collegare vue e creare ciò che mi serve -->
+<script>
+import { computed } from 'vue';
+
+export default {
+  data() {
+    return {
+      title: 'primo progetto vite'
+    };
+
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+
+
+
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- dentro il template creo un h1 dove collegare il js -->
+  <h1>{{ title }}</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
